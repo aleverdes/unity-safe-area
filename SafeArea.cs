9091d5cc068d77fc9be757a43e35d57f7fb4f20f
Alexander Travkin <alexander.travkin@varwin.com>
2022-06-12 16:42:10 +0300

    Supported CanvasScaler Expand ScreenMatchMode

--- SafeArea.cs
+++ SafeArea.cs
@@ -20,27 +20,48 @@ namespace AffenCode
             Process();
         }
 
-        [ContextMenu("Process")]
         private void Process()
         {
             var referenceResolutionAspect = Vector2.one;
             var aspectRatio = 1f;
 
             _canvasScaler = GetComponentInParent<CanvasScaler>();
-            if (_canvasScaler && _canvasScaler.screenMatchMode == CanvasScaler.ScreenMatchMode.MatchWidthOrHeight)
+
+            if (!_canvasScaler)
+            {
+                Debug.LogError("SafeArea required CanvasScaler");
+                return;
+            }
+            
+            if (_canvasScaler.screenMatchMode == CanvasScaler.ScreenMatchMode.MatchWidthOrHeight)
             {
                 var screen = new Vector2(Screen.width, Screen.height);
                 var referenceResolution = _canvasScaler.referenceResolution;
                 referenceResolutionAspect.x = referenceResolution.x / screen.x;
                 referenceResolutionAspect.y = referenceResolution.y / screen.y;
                 aspectRatio = Mathf.Lerp(referenceResolutionAspect.x, referenceResolutionAspect.y, _canvasScaler.matchWidthOrHeight);
-            }
 
-            _rectTransform.anchorMin = Vector2.zero;
-            _rectTransform.anchorMax = Vector2.zero;
-            _rectTransform.pivot = Vector2.zero;
-            _rectTransform.anchoredPosition = aspectRatio * new Vector2(Screen.safeArea.x, Screen.safeArea.y);
-            _rectTransform.sizeDelta = aspectRatio * new Vector2(Screen.safeArea.width, Screen.safeArea.height);
+                _rectTransform.anchorMin = Vector2.zero;
+                _rectTransform.anchorMax = Vector2.zero;
+                _rectTransform.pivot = Vector2.zero;
+                _rectTransform.anchoredPosition = aspectRatio * new Vector2(Screen.safeArea.x, Screen.safeArea.y);
+                _rectTransform.sizeDelta = aspectRatio * new Vector2(Screen.safeArea.width, Screen.safeArea.height);
+            }
+            else if (_canvasScaler.screenMatchMode == CanvasScaler.ScreenMatchMode.Expand)
+            {
+                var canvas = GetComponentInParent<Canvas>();
+                var canvasScale = canvas.transform.localScale;
+                
+                var screen = new Vector2(Screen.width, Screen.height);
+                var referenceResolution = _canvasScaler.referenceResolution;
+                var safeArea = Screen.safeArea;
+                
+                _rectTransform.anchorMin = Vector2.zero;
+                _rectTransform.anchorMax = Vector2.zero;
+                _rectTransform.pivot = Vector2.zero;
+                _rectTransform.anchoredPosition = new Vector2(Screen.safeArea.x / canvasScale.x, Screen.safeArea.y / canvasScale.y);
+                _rectTransform.sizeDelta = new Vector2(Screen.safeArea.width / canvasScale.x, Screen.safeArea.height / canvasScale.y);
+            }
         }
     }
 }
